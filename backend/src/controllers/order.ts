6f7cafd2f8ff69a02d418e85d81264da6e3dcab6
Daniel Shahovtsov <daniil.shahovtsov@yandex.ru>
2025-07-11 13:33:59 +0300

    feat/Санитаризация коммента

--- backend/src/controllers/order.ts
+++ backend/src/controllers/order.ts
@@ -5,6 +5,7 @@ import NotFoundError from '../errors/not-found-error'
 import Order, { IOrder } from '../models/order'
 import Product, { IProduct } from '../models/product'
 import User from '../models/user'
+import sanitizeHTML from 'sanitize-html'
 
 // eslint-disable-next-line max-len
 // GET /orders?page=2&limit=5&sort=totalAmount&order=desc&orderDateFrom=2024-07-01&orderDateTo=2024-08-01&status=delivering&totalAmountFrom=100&totalAmountTo=1000&search=%2B1
@@ -321,13 +322,17 @@ export const createOrder = async (
             return next(new BadRequestError('Неверная сумма заказа'))
         }
 
+        const clean = sanitizeHTML(comment, {
+            allowedTags: ['b', 'img', 'div']
+        })
+
         const newOrder = new Order({
             totalAmount: total,
             products: items,
             payment,
             phone,
             email,
-            comment,
+            comment: clean,
             customer: userId,
             deliveryAddress: address,
         })
