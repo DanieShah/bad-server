02b518df4502f463dbd6e2e810976cbbd7de7347
Daniel Shahovtsov <daniil.shahovtsov@yandex.ru>
2025-07-12 11:20:29 +0300

    feat/rateLimit

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,6 +9,7 @@ import { DB_ADDRESS } from './config'
 import errorHandler from './middlewares/error-handler'
 import serveStatic from './middlewares/serverStatic'
 import routes from './routes'
+import { rateLimit } from 'express-rate-limit'
 
 const { PORT = 3000 } = process.env
 const app = express()
@@ -28,6 +29,16 @@ app.options('*', cors())
 app.use(routes)
 app.use(errors())
 app.use(errorHandler)
+
+app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 минут
+      max: 40,
+      message: 'Слишком много запросов с этого IP, попробуйте позже',
+      standardHeaders: true,
+      legacyHeaders: false,
+    })
+  )
 
 // eslint-disable-next-line no-console
 
